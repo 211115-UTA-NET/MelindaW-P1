2919c7478fc1f64b1fbe223f377756a943d9d64c
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-29 12:45:28 -0600

    Added a UI

--- PlainOldStoreApp/PlainOldStoreApp.Ui/Program.cs
+++ PlainOldStoreApp/PlainOldStoreApp.Ui/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PlainOldStoreApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            bool isRunning = true;
+            string connectionString = File.ReadAllText(
+                "C:/Users/melin/OneDrive/Desktop/RevGit/MelindaW-P0/waggonerm-posa-db.txt");
+            Console.WriteLine("Welcome to Plain Old Store!");
+            Console.WriteLine("What can I help you with today?");
+            Console.WriteLine();
+
+            while (isRunning)
+            {
+                Console.WriteLine("Please select one of the following menu options.");
+                Console.WriteLine("1. Place Order\n2. Add Customer\n3. Lookup Order\n4. Exit");
+                string? slection = Console.ReadLine()?.Trim().ToLower();
+                Console.WriteLine();
+                isRunning = MainMenu(slection, isRunning);
+            }
+        }
+        internal static bool MainMenu(string? slection, bool isRunning)
+        {
+            switch (slection)
+            {
+                case "1":
+                case "place order":
+                    //PlaneOldShop.PlaceOrder(connectionString);
+                    break;
+                case "2":
+                case "add customer":
+                    //PlaneOldShop.AddCustomer(connectionString);
+                    break;
+                case "3":
+                case "lookup order":
+                    //PlaneOldShop.LookupOrder(connectionString);
+                    break;
+                case "4":
+                case "exit":
+                    Console.WriteLine("Have a nice day. Goodbye!");
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please select a valid menu option.");
+                    break;
+            }
+            return isRunning;
+        }
+    }
+}
