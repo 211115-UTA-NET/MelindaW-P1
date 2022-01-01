e9a31088e235dccf0647caa6154c9f04199aca72
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-01 02:26:49 -0600

    Added the ICustomerRepository and updated it to be asynchronous

---
+++ PlainOldStoreApp/PlainOldStoreApp.DataStorage/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace PlainOldStoreApp.DataStorage
+{
+    public interface ICustomerRepository
+    {
+        Task<bool> GetCustomerEmail(string? email);
+
+        Task<List<Customer>> GetAllCustomer(string? firstName, string? lasName);
+
+        Task<bool> AddNewCustomer(
+            string? firstName,
+            string? lastName,
+            string? address1,
+            string? city,
+            string? state,
+            string? zip,
+            string email);
+
+        Task<Guid> SqlGetCustomerId(string email);
+    }
+}
