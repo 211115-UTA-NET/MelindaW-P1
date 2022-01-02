62347190b958d8baf6984ab21c4bc029c71d9c63
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-01 23:46:02 -0600

    Added and updated to be accessible to other classes and get the result of an Task

---
+++ PlainOldStoreApp/PlainOldStoreApp.DataStorage/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace PlainOldStoreApp.DataStorage
+{
+    public class Product
+    {
+        public int? ProductId { get; }
+        public string? ProductName { get; }
+        public string? ProductDescription { get; }
+        public decimal ProductPrice { get; }
+        public int? ProductQuantiy { get; set; }
+        public int StoreID { get; }
+
+        private readonly IProductRepository _productRepository;
+
+        public Product(int storeLocation, IProductRepository productRepository)
+        {
+            StoreID = storeLocation;
+            _productRepository = productRepository;
+        }
+        public Product(int productId, string productName, string productDescription, decimal productPrice, int productQuantiy, int storeID)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            ProductDescription = productDescription;
+            ProductPrice = productPrice;
+            ProductQuantiy = productQuantiy;
+            StoreID = storeID;
+        }
+
+        public List<Product> GetStoreInventory()
+        {
+            List<Product> products = _productRepository.GetAllStoreProducts(StoreID).Result;
+            return products;
+        }
+    }
+}
