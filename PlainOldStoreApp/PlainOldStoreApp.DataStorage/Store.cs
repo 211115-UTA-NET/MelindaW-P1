85f51560b2a4ac62ab53adc9c3e8f304a287cb3c
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-01 23:49:25 -0600

    Added and updated to be accessible to other classes and get the result of an Task

---
+++ PlainOldStoreApp/PlainOldStoreApp.DataStorage/Store.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.DataStorage
+{
+    public class Store
+    {
+        private Dictionary<int, string> _stores = new Dictionary<int, string>();
+
+        public string GetStore(int key)
+        {
+            string value = "";
+            if (_stores.ContainsKey(key))
+            {
+                value = _stores[key];
+            }
+            return value;
+        }
+
+        private readonly IStoreRepository _storeRepository;
+
+        public Store(IStoreRepository storeRepository)
+        {
+            _storeRepository = storeRepository;
+        }
+
+        public Dictionary<int, string> GetStoresFromDatabase()
+        {
+            _stores = _storeRepository.RetriveStores().Result;
+            return _stores;
+        }
+    }
+}
