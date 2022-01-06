5137a2ae16ded1cfaa638f2492dce9ddb09e70ee
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-06 13:03:04 -0600

    Added IStoreService and StoreService.

---
+++ PlainOldStoreApp.Ui/PlainOldStoreApp.Ui/StoreService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.Ui
+{
+    internal class StoreService : IStoreService
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public StoreService(Uri serverUri)
+        {
+            _httpClient.BaseAddress = serverUri;
+        }
+        public Task<Dictionary<int, string>> GetStoreListAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
