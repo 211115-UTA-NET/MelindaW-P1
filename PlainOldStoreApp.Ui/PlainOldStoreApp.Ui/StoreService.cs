c7c0441fc15152b1e7cee634876d1f639116cfff
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-07 17:58:40 -0600

    Method not revices stores from api

--- PlainOldStoreApp.Ui/PlainOldStoreApp.Ui/StoreService.cs
+++ PlainOldStoreApp.Ui/PlainOldStoreApp.Ui/StoreService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +16,40 @@ namespace PlainOldStoreApp.Ui
         {
             _httpClient.BaseAddress = serverUri;
         }
-        public Task<Dictionary<int, string>> GetStoreListAsync()
+        public async Task<Dictionary<int, string>> GetStoreListAsync()
         {
-            throw new NotImplementedException();
+            string requestUri = "/api/Store/stores";
+
+            HttpRequestMessage storesRequest = new(HttpMethod.Get, requestUri);
+
+            storesRequest.Headers.Accept.Add(new(MediaTypeNames.Application.Json));
+
+            HttpResponseMessage storesResponse;
+
+            try
+            {
+                storesResponse = await _httpClient.SendAsync(storesRequest);
+            }
+            catch (ServerException ex)
+            {
+                throw new ServerException("network error", ex);
+            }
+
+            storesResponse.EnsureSuccessStatusCode();
+
+            if (storesResponse.Content.Headers.ContentType?.MediaType != MediaTypeNames.Application.Json)
+            {
+                throw new ServerException();
+            }
+
+            Dictionary<int, string>? stores = await storesResponse.Content.ReadFromJsonAsync<Dictionary<int, string>>();
+
+            if (stores == null)
+            {
+                throw new ServerException();
+            }
+
+            return stores;
         }
     }
 }
