c59b4619531bfe68334fe4d261eb8a08c9ee33b8
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-07 18:28:12 -0600

    Added order dto

---
+++ PlainOldStoreApp.Ui/PlainOldStoreApp.Ui/Dtos/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.Ui.Dtos
+{
+    internal class Order
+    {
+        public int ProductId { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductQuantiy { get; set; }
+    }
+}
