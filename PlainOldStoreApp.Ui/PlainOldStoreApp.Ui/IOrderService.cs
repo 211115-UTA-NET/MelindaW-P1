2ee3c3140d4c9cc4bfbb9b1ea68d762269fc230d
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-07 18:27:36 -0600

    Added IOrderService

---
+++ PlainOldStoreApp.Ui/PlainOldStoreApp.Ui/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using PlainOldStoreApp.Ui.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainOldStoreApp.Ui
+{
+    internal interface IOrderService
+    {
+        Task<Tuple<List<Order>, string>> PostAllOrders(int customerId, int storeLocation, List<Order> ordersMade);
+    }
+}
