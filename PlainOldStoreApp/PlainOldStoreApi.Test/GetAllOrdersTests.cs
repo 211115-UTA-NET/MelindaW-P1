81c8ffd17e154e4268f33cee1236a5dbe0abcad3
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-01-10 11:45:45 -0600

    Added test for Getting all stores using moq

---
+++ PlainOldStoreApp/PlainOldStoreApi.Test/GetAllOrdersTests.cs
@@ -0,0 +1,33 @@
+using Moq;
+using PlainOldStoreApi.Api.Controllers;
+using PlainOldStoreApp.DataStorage;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PlainOldStoreApi.Test
+{
+    public class GetAllOrdersTests
+    {
+        [Fact]
+        public void GetAllStoresTest()
+        {
+            //Arrange
+            Dictionary<int, string> stores = new()
+            {
+                { 1, "Mountain View" },
+                { 2, "San Jose"},
+                { 3, "Edmond"}
+            };
+            var mock = new Mock<IStoreRepository>();
+            mock.Setup(x => x.RetriveStores()).ReturnsAsync(stores);
+            var controller = new StoreController(mock.Object);
+
+            //Act
+            var actual = controller.GetAllStores();
+            //Assert
+            Assert.Equal(stores.Keys, actual.Result.Value?.Keys);
+            Assert.Equal(stores.Values, actual.Result.Value?.Values);
+        }
+    }
+}
